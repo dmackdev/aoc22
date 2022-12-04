fc645b49e2745f3572bffb2931225327cbbdc22c
dmackdev <dmackdev@gmail.com>
2022-12-04 15:32:39 +0000

    Day 4 part 2 implementation.

--- day4/src/lib.rs
+++ day4/src/lib.rs
@@ -10,9 +10,18 @@ impl Range {
     fn contains(&self, other: &Range) -> bool {
         self.lower_bound <= other.lower_bound && self.upper_bound >= other.upper_bound
     }
+
+    fn overlaps(&self, other: &Range) -> bool {
+        let self_range = self.lower_bound..=self.upper_bound;
+        let other_range = other.lower_bound..=other.upper_bound;
+        self_range.contains(&other.lower_bound)
+            || self_range.contains(&other.upper_bound)
+            || other_range.contains(&self.lower_bound)
+            || other_range.contains(&self.upper_bound)
+    }
 }
 
-pub fn parse_input(input: &str) -> usize {
+pub fn count_fully_contained(input: &str) -> usize {
     input
         .lines()
         .filter_map(|line| {
@@ -22,6 +31,17 @@ pub fn parse_input(input: &str) -> usize {
                 range_1.contains(&range_2) || range_2.contains(&range_1);
 
             is_one_pair_fully_contained.then_some((range_1, range_2))
+        })
+        .count()
+}
+
+pub fn count_overlaps(input: &str) -> usize {
+    input
+        .lines()
+        .filter_map(|line| {
+            let (range_1, range_2) = parse_ranges_from_line(line);
+
+            range_1.overlaps(&range_2).then_some((range_1, range_2))
         })
         .count()
 }
@@ -49,9 +69,10 @@ fn parse_ranges_from_line(line: &str) -> (Range, Range) {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
 
     #[test]
-    fn parse_input_simple_case() {
+    fn count_fully_contained_simple_case() {
         let input = "\
 2-4,6-8
 2-3,4-5
@@ -60,6 +81,32 @@ mod tests {
 6-6,4-6
 2-6,4-8";
 
-        assert_eq!(parse_input(input), 2);
+        assert_eq!(count_fully_contained(input), 2);
+    }
+
+    #[test]
+    fn count_overlaps_simple_case() {
+        let input = "\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+
+        assert_eq!(count_overlaps(input), 4);
+    }
+
+    #[rstest]
+    #[case("2-4,6-8", false)]
+    #[case("2-3,4-5", false)]
+    #[case("5-7,7-9", true)]
+    #[case("2-8,3-7", true)]
+    #[case("6-6,4-6", true)]
+    #[case("2-6,4-8", true)]
+    fn range_overlaps_case(#[case] line: &str, #[case] expected: bool) {
+        let (range_1, range_2) = parse_ranges_from_line(line);
+
+        assert_eq!(range_1.overlaps(&range_2), expected);
     }
 }
--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,14 +1,21 @@
 use std::fs;
 
-use day4::parse_input;
+use day4::{count_fully_contained, count_overlaps};
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
 
-    let count = parse_input(&contents);
+    let count = count_fully_contained(&contents);
 
     println!(
         "The number of assignment pairs where one range fully contains the other: {}",
+        count
+    );
+
+    let count = count_overlaps(&contents);
+
+    println!(
+        "The number of assignment pairs where one range fully overlaps the other: {}",
         count
     );
 }
