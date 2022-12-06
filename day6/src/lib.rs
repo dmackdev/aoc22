d21743a2da03526b468d2708bd51a0e168633249
dmackdev <dmackdev@gmail.com>
2022-12-06 09:18:23 +0000

    Day 6 part 2 implementation.

--- day6/src/lib.rs
+++ day6/src/lib.rs
@@ -1,12 +1,10 @@
 use std::collections::HashSet;
 
-const WINDOW_SIZE: usize = 4;
-
-pub fn find_marker_index(input: &str) -> usize {
+pub fn find_marker_index(input: &str, window_size: usize) -> usize {
     for (idx, group) in input
         .chars()
         .collect::<Vec<char>>()
-        .windows(WINDOW_SIZE)
+        .windows(window_size)
         .enumerate()
     {
         let mut set: HashSet<char> = HashSet::new();
@@ -14,8 +12,8 @@ pub fn find_marker_index(input: &str) -> usize {
             set.insert(*c);
         }
 
-        if set.len() == WINDOW_SIZE {
-            return idx + WINDOW_SIZE;
+        if set.len() == window_size {
+            return idx + window_size;
         }
     }
     panic!("Could not find marker index.");
@@ -31,8 +29,19 @@ mod tests {
     #[case("nppdvjthqldpwncqszvftbrmjlhg", 6)]
     #[case("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
     #[case("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
-    fn find_marker_index_case(#[case] line: &str, #[case] expected: usize) {
-        let idx = find_marker_index(line);
+    fn find_packet_marker_index_window_size_4_case(#[case] line: &str, #[case] expected: usize) {
+        let idx = find_marker_index(line, 4);
+        assert_eq!(idx, expected);
+    }
+
+    #[rstest]
+    #[case("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+    #[case("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+    #[case("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+    #[case("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+    #[case("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+    fn find_packet_marker_index_window_size_14_case(#[case] line: &str, #[case] expected: usize) {
+        let idx = find_marker_index(line, 14);
         assert_eq!(idx, expected);
     }
 }
--- day6/src/main.rs
+++ day6/src/main.rs
@@ -5,7 +5,17 @@ use day6::find_marker_index;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Should have been able to read the file");
 
-    let marker_index = find_marker_index(&input);
+    let marker_index = find_marker_index(&input, 4);
 
-    println!("The first start-of-packet marker index is {}", marker_index);
+    println!(
+        "The first start-of-packet marker index (window size 4) is {}",
+        marker_index
+    );
+
+    let marker_index = find_marker_index(&input, 14);
+
+    println!(
+        "The first start-of-message marker index (window size 14) is {}",
+        marker_index
+    );
 }
