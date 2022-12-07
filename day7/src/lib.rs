085ca707e668bb4ddc2981779677ab1676aaa596
dmackdev <dmackdev@gmail.com>
2022-12-07 15:47:48 +0000

    Day 7 part 2 implementation.

--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -4,7 +4,7 @@ use std::rc::Rc;
 
 #[derive(Debug, Clone)]
 pub struct Directory {
-    path: String,
+    pub path: String,
     files: Vec<Rc<File>>,
     indirect_files: Vec<Rc<File>>,
 }
@@ -17,18 +17,11 @@ pub struct File {
 
 impl Directory {
     pub fn size(&self) -> u128 {
-        let sum = self
-            .files
+        self.files
             .iter()
             .chain(self.indirect_files.iter())
             .map(|f| f.size)
-            .sum();
-
-        if sum > 100000 {
-            0
-        } else {
-            sum
-        }
+            .sum()
     }
 }
 
@@ -125,6 +118,46 @@ pub fn parse_input(input: &str) -> Vec<Directory> {
     fs_map.values().cloned().collect()
 }
 
+pub fn sum_dirs_sizes_with_limit(dirs: &[Directory], limit: u128) -> u128 {
+    dirs.iter()
+        .filter_map(|n| {
+            let size = n.size();
+            if size > limit {
+                None
+            } else {
+                Some(size)
+            }
+        })
+        .sum()
+}
+
+pub fn find_smallest_dir_to_delete(
+    dirs: &[Directory],
+    max_fs_space: u128,
+    required_space: u128,
+) -> &Directory {
+    let root_dir = dirs.iter().find(|dir| dir.path == "/").unwrap();
+    let total_used_space = root_dir.size();
+    let total_unused_space = max_fs_space - total_used_space;
+    let total_space_to_delete = required_space - total_unused_space;
+
+    let mut possible_dirs: Vec<_> = dirs
+        .iter()
+        .filter_map(|dir| {
+            let size = dir.size();
+            if size >= total_space_to_delete {
+                Some((size, dir))
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    possible_dirs.sort_by_key(|(size, _)| *size);
+
+    possible_dirs.first().unwrap().1
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -137,8 +170,12 @@ mod tests {
 
         let dirs = parse_input(&input);
 
-        let sum: u128 = dirs.iter().map(|n| n.size()).sum();
+        let sum: u128 = sum_dirs_sizes_with_limit(&dirs, 100000);
 
         assert_eq!(sum, 95437);
+
+        let dir_to_delete = find_smallest_dir_to_delete(&dirs, 70000000, 30000000);
+
+        assert_eq!(dir_to_delete.path, "/d");
     }
 }
--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,13 +1,21 @@
 use std::fs;
 
-use day7::parse_input;
+use day7::{find_smallest_dir_to_delete, parse_input, sum_dirs_sizes_with_limit};
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Should have been able to read the file");
 
     let dirs = parse_input(&input);
 
-    let sum: u128 = dirs.iter().map(|n| n.size()).sum();
+    let sum: u128 = sum_dirs_sizes_with_limit(&dirs, 100000);
 
     println!("Total size of directories: {}", sum);
+
+    let dir_to_delete = find_smallest_dir_to_delete(&dirs, 70000000, 30000000);
+
+    println!(
+        "Smallest dir to delete: {}, size: {}",
+        dir_to_delete.path,
+        dir_to_delete.size()
+    );
 }
