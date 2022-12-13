d3a71baa5401b9dda26ee50bedafa9caa8c85d99
dmackdev <dmackdev@gmail.com>
2022-12-13 14:40:12 +0000

    Day 13 part 2 implementation.

--- day13/src/lib.rs
+++ day13/src/lib.rs
@@ -11,19 +11,50 @@ pub struct Packet {
     data: Vec<PacketData>,
 }
 
-pub fn parse_input(input: &str) -> usize {
+pub fn parse_packets(input: &str) -> Vec<Packet> {
     input
         .lines()
         .collect::<Vec<_>>()
         .chunks(3)
-        .enumerate()
-        .filter_map(|(idx, chunk)| {
+        .flat_map(|chunk| {
             let packet_1 = chunk[0].parse::<Packet>().unwrap();
             let packet_2 = chunk[1].parse::<Packet>().unwrap();
+            [packet_1, packet_2]
+        })
+        .collect()
+}
+
+pub fn sum_indices_of_ordered_packet_pairs(packets: &[Packet]) -> usize {
+    packets
+        .chunks(2)
+        .enumerate()
+        .filter_map(|(idx, chunk)| {
+            let packet_1 = &chunk[0];
+            let packet_2 = &chunk[1];
 
             (packet_1 < packet_2).then_some(idx + 1)
         })
         .sum()
+}
+
+pub fn get_decoder_key(mut packets: Vec<Packet>, divider_packets: Vec<Packet>) -> usize {
+    packets.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+    divider_packets
+        .into_iter()
+        .map(|divider_p| insert_packet_sorted(&mut packets, divider_p) + 1)
+        .product()
+}
+
+fn insert_packet_sorted(sorted_packets: &mut Vec<Packet>, packet: Packet) -> usize {
+    let insertion_idx = sorted_packets
+        .iter()
+        .position(|other| matches!(packet.partial_cmp(other), Some(Ordering::Less)))
+        .unwrap();
+
+    sorted_packets.insert(insertion_idx, packet);
+
+    insertion_idx
 }
 
 impl FromStr for Packet {
@@ -158,8 +189,15 @@ mod tests {
 
     #[test]
     fn example() {
-        let result = parse_input(&fs::read_to_string("test_input.txt").unwrap());
+        let packets = parse_packets(&fs::read_to_string("test_input.txt").unwrap());
+        let result = sum_indices_of_ordered_packet_pairs(&packets);
 
-        assert_eq!(result, 13)
+        assert_eq!(result, 13);
+
+        let divider_packets = vec!["[[2]]".parse().unwrap(), "[[6]]".parse().unwrap()];
+
+        let decoder_key = get_decoder_key(packets, divider_packets);
+
+        assert_eq!(decoder_key, 140);
     }
 }
--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,12 +1,19 @@
 use std::fs;
 
-use day13::parse_input;
+use day13::{get_decoder_key, parse_packets, sum_indices_of_ordered_packet_pairs};
 
 fn main() {
-    let result = parse_input(&fs::read_to_string("input.txt").unwrap());
+    let packets = parse_packets(&fs::read_to_string("input.txt").unwrap());
+    let indices_sum = sum_indices_of_ordered_packet_pairs(&packets);
 
     println!(
         "The sum of the indices of the packets pairs in the correct order is: {}",
-        result
+        indices_sum
     );
+
+    let divider_packets = vec!["[[2]]".parse().unwrap(), "[[6]]".parse().unwrap()];
+
+    let decoder_key = get_decoder_key(packets, divider_packets);
+
+    println!("The decoder key is: {}", decoder_key);
 }
