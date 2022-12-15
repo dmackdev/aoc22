328f677bdd8434660ddcdadb83a8118cc414773d
dmackdev <dmackdev@gmail.com>
2022-12-15 12:54:53 +0000

    Refactor

--- day15/src/lib.rs
+++ day15/src/lib.rs
@@ -1,8 +1,8 @@
 use regex::Regex;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-#[derive(Debug)]
-struct Pos {
+#[derive(Debug, PartialEq, Eq)]
+pub struct Pos {
     x: i32,
     y: i32,
 }
@@ -17,12 +17,10 @@ impl Pos {
     }
 }
 
-pub fn get_impossible_xs_for_row(input: &str, row: i32) -> Vec<i32> {
+pub fn parse_occupied_positions(input: &str) -> HashMap<i32, Vec<(i32, i32)>> {
     let re = Regex::new(r"x=(-?\d+), y=(-?\d+)").unwrap();
 
-    let mut sensor_xs = vec![];
-    let mut beacon_xs = vec![];
-    let mut impossible_xs = HashSet::new();
+    let mut occupied_map: HashMap<i32, Vec<(i32, i32)>> = HashMap::new();
 
     for line in input.lines() {
         for pair in re
@@ -39,32 +37,58 @@ pub fn get_impossible_xs_for_row(input: &str, row: i32) -> Vec<i32> {
             let sensor_pos = &pair[0];
             let beacon_pos = &pair[1];
 
-            if sensor_pos.y == row {
-                sensor_xs.push(sensor_pos.x);
-            }
-
-            if beacon_pos.y == row {
-                beacon_xs.push(beacon_pos.x);
-            }
-
             let distance_to_beacon = sensor_pos.distance_to(beacon_pos);
 
-            let distance_to_row = sensor_pos.distance_to(&Pos::new(sensor_pos.x, row));
+            occupied_map
+                .entry(sensor_pos.y)
+                .and_modify(|row| row.push((sensor_pos.x, distance_to_beacon)))
+                .or_insert_with(|| vec![(sensor_pos.x, distance_to_beacon)]);
 
-            if distance_to_row <= distance_to_beacon {
-                let row_range = sensor_pos.x - distance_to_beacon + distance_to_row
-                    ..=sensor_pos.x + distance_to_beacon - distance_to_row;
+            occupied_map
+                .entry(beacon_pos.y)
+                .and_modify(|row| row.push((beacon_pos.x, 0)))
+                .or_insert_with(|| vec![(beacon_pos.x, 0)]);
+        }
+    }
 
+    occupied_map
+}
+
+pub fn get_impossible_positions_for_row(
+    occupied_positions: HashMap<i32, Vec<(i32, i32)>>,
+    row: i32,
+) -> Vec<Pos> {
+    let mut impossible_xs = HashSet::new();
+
+    for (y, occupants) in occupied_positions.iter() {
+        for (x, distance_to_beacon) in occupants {
+            let distance_to_row = Pos::new(*x, *y).distance_to(&Pos::new(*x, row));
+
+            if distance_to_row <= *distance_to_beacon {
+                let row_range = x - distance_to_beacon + distance_to_row
+                    ..=x + distance_to_beacon - distance_to_row;
                 impossible_xs.extend(row_range.into_iter());
             }
         }
     }
-    let xs_to_remove = sensor_xs.iter().chain(beacon_xs.iter()).collect::<Vec<_>>();
+
+    let xs_to_remove = occupied_positions
+        .get(&row)
+        .unwrap_or(&Vec::new())
+        .iter()
+        .map(|p| p.0)
+        .collect::<Vec<_>>();
 
     impossible_xs
         .into_iter()
-        .filter(|x| !xs_to_remove.contains(&x))
+        .filter(|x| !xs_to_remove.contains(x))
+        .map(|x| Pos::new(x, row))
         .collect::<Vec<_>>()
+}
+
+fn find_position(impossible_positions: Vec<Pos>, max: i32) -> Option<Pos> {
+    for row in 0..=max {}
+    todo!()
 }
 
 #[cfg(test)]
@@ -75,8 +99,13 @@ mod tests {
 
     #[test]
     fn example() {
-        let result = get_impossible_xs_for_row(&fs::read_to_string("test_input.txt").unwrap(), 10);
+        let occupied_positions =
+            parse_occupied_positions(&fs::read_to_string("test_input.txt").unwrap());
 
-        assert_eq!(result.len(), 26);
+        let impossible_positions = get_impossible_positions_for_row(occupied_positions, 10);
+
+        assert_eq!(impossible_positions.len(), 26);
+
+        // assert_eq!(find_position(impossible_positions), Some(Pos::new(14, 11)));
     }
 }
--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,14 +1,16 @@
 use std::fs;
 
-use day15::get_impossible_xs_for_row;
+use day15::{get_impossible_positions_for_row, parse_occupied_positions};
 
 fn main() {
     let row = 2000000;
-    let result = get_impossible_xs_for_row(&fs::read_to_string("input.txt").unwrap(), row);
+    let occupied_positions = parse_occupied_positions(&fs::read_to_string("input.txt").unwrap());
+
+    let impossible_positions = get_impossible_positions_for_row(occupied_positions, row);
 
     println!(
         "The number of impossible beacon positions for y={} is: {}",
         row,
-        result.len()
+        impossible_positions.len()
     )
 }
