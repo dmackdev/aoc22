8331ad16d430f06b4640906309784e2a8a4cde05
dmackdev <dmackdev@gmail.com>
2022-12-15 14:58:27 +0000

    Part 2 attempt

--- day15/src/lib.rs
+++ day15/src/lib.rs
@@ -55,8 +55,9 @@ pub fn parse_occupied_positions(input: &str) -> HashMap<i32, Vec<(i32, i32)>> {
 }
 
 pub fn get_impossible_positions_for_row(
-    occupied_positions: HashMap<i32, Vec<(i32, i32)>>,
+    occupied_positions: &HashMap<i32, Vec<(i32, i32)>>,
     row: i32,
+    remove_all_occupied: bool,
 ) -> Vec<Pos> {
     let mut impossible_xs = HashSet::new();
 
@@ -72,23 +73,85 @@ pub fn get_impossible_positions_for_row(
         }
     }
 
-    let xs_to_remove = occupied_positions
-        .get(&row)
-        .unwrap_or(&Vec::new())
+    let xs_to_remove = match occupied_positions.get(&row) {
+        Some(v) => v.to_owned(),
+        None => vec![],
+    };
+
+    let xs_to_remove = xs_to_remove
         .iter()
-        .map(|p| p.0)
+        .filter_map(|(x, distance)| {
+            if remove_all_occupied {
+                return Some(x);
+            }
+
+            if *distance == 0 {
+                Some(x)
+            } else {
+                None
+            }
+        })
         .collect::<Vec<_>>();
 
     impossible_xs
         .into_iter()
-        .filter(|x| !xs_to_remove.contains(x))
+        .filter(|x| !xs_to_remove.contains(&x))
         .map(|x| Pos::new(x, row))
         .collect::<Vec<_>>()
 }
 
-fn find_position(impossible_positions: Vec<Pos>, max: i32) -> Option<Pos> {
-    for row in 0..=max {}
-    todo!()
+pub fn find_position(occupied_positions: &HashMap<i32, Vec<(i32, i32)>>, max: i32) -> Option<Pos> {
+    for row in 0..=max {
+        println!("Row {}", row);
+
+        let impossible_positions_for_row =
+            get_impossible_positions_for_row(occupied_positions, row, true);
+
+        let impossible_positions_for_row = impossible_positions_for_row
+            .into_iter()
+            .filter(|pos| 0 <= pos.x && pos.x <= max)
+            .collect::<Vec<_>>();
+
+        let num_occupied_positions_in_range = occupied_positions
+            .get(&row)
+            .unwrap_or(&Vec::new())
+            .iter()
+            .filter(|(x, _)| 0 <= *x && *x <= max)
+            .count() as i32;
+
+        if impossible_positions_for_row.len() as i32 + num_occupied_positions_in_range < max + 1 {
+            let full_range_set = HashSet::<i32>::from_iter(0..=max);
+            let impossible_positions_set =
+                HashSet::<i32>::from_iter(impossible_positions_for_row.iter().map(|p| p.x));
+            let diff = full_range_set
+                .difference(&impossible_positions_set)
+                .collect::<HashSet<&i32>>();
+            let empty = Vec::new();
+            let occupied_set = HashSet::from_iter(
+                occupied_positions
+                    .get(&row)
+                    .unwrap_or(&empty)
+                    .iter()
+                    .map(|(x, _)| x),
+            );
+            let diff = diff.difference(&occupied_set).collect::<Vec<_>>();
+
+            let x = diff.first();
+
+            return Some(Pos::new(***x.unwrap(), row));
+        }
+    }
+    None
+}
+
+pub fn calculate_tuning_frequency(pos: Pos, max: i32) -> i32 {
+    pos.x * max + pos.y
+}
+
+fn print_xs(positions: &[Pos]) {
+    let mut xs = positions.iter().map(|p| p.x).collect::<Vec<_>>();
+    xs.sort();
+    println!("{:?}", xs);
 }
 
 #[cfg(test)]
@@ -97,15 +160,40 @@ mod tests {
 
     use super::*;
 
-    #[test]
-    fn example() {
+    fn assert_positions(positions: &[Pos], expected_xs: &[i32], expected_y: i32) {
+        positions
+            .iter()
+            .for_each(|pos| assert_eq!(pos.y, expected_y));
+
+        let mut xs = positions.iter().map(|pos| pos.x).collect::<Vec<_>>();
+        xs.sort();
+
+        assert_eq!(xs, expected_xs);
+    }
+
+    use rstest::rstest;
+    #[rstest]
+    #[case(9, &[-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,])]
+    #[case(10, &[-2, -1, 0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24])]
+    #[case(11, &[-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])]
+    fn example_impossible_positions_for_row(#[case] row: i32, #[case] expected: &[i32]) {
         let occupied_positions =
             parse_occupied_positions(&fs::read_to_string("test_input.txt").unwrap());
 
-        let impossible_positions = get_impossible_positions_for_row(occupied_positions, 10);
+        let impossible_positions =
+            get_impossible_positions_for_row(&occupied_positions, row, false);
 
-        assert_eq!(impossible_positions.len(), 26);
+        assert_positions(&impossible_positions, expected, row);
+    }
 
-        // assert_eq!(find_position(impossible_positions), Some(Pos::new(14, 11)));
+    #[test]
+    fn example_find_single_position() {
+        let occupied_positions =
+            parse_occupied_positions(&fs::read_to_string("test_input.txt").unwrap());
+
+        let position = find_position(&occupied_positions, 20).unwrap();
+
+        assert_eq!(position, Pos::new(14, 11));
+        assert_eq!(calculate_tuning_frequency(position, 4000000), 56000011)
     }
 }
--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,16 +1,29 @@
 use std::fs;
 
-use day15::{get_impossible_positions_for_row, parse_occupied_positions};
+use day15::{
+    calculate_tuning_frequency, find_position, get_impossible_positions_for_row,
+    parse_occupied_positions,
+};
 
 fn main() {
     let row = 2000000;
     let occupied_positions = parse_occupied_positions(&fs::read_to_string("input.txt").unwrap());
 
-    let impossible_positions = get_impossible_positions_for_row(occupied_positions, row);
+    let impossible_positions = get_impossible_positions_for_row(&occupied_positions, row, false);
 
     println!(
         "The number of impossible beacon positions for y={} is: {}",
         row,
         impossible_positions.len()
-    )
+    );
+
+    let max = 4000000;
+    let position = find_position(&occupied_positions, max).unwrap();
+
+    println!("The position is {:?}", position);
+
+    println!(
+        "Tuning frequency: {}",
+        calculate_tuning_frequency(position, max)
+    );
 }
